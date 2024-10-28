1d888f58de169f1d1c1f70d16d21584d206139f9
Sergio Bocci <powwerie@gmail.com>
2024-10-28 14:39:36 -0600

    yes

---
+++ Week_10/mon.cpp
@@ -0,0 +1,9 @@
+//user
+//defined types
+//-composite data types
+/*
+C++
+struct - data only
+class - data and methods to operate on the data
+grouping multiple data elements of different types together
+*/
