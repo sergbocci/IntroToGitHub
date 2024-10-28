4db42940e43f6a5d9f8506f8f56d0287b33f7ddd
Sergio Bocci <powwerie@gmail.com>
2024-10-28 14:43:27 -0600

    class

--- Week_10/mon.cpp
+++ Week_10/mon.cpp
@@ -6,4 +6,10 @@ C++
 struct - data only
 class - data and methods to operate on the data
 grouping multiple data elements of different types together
+
+Ex: Baseball data. Create a struct data type to represent a baseball player.
+definition - what is included in the type
+declaration - creating an instance of the type
+population - setting the values for each instance
+access - how do we read variable values
 */
