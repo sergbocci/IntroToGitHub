ae761a829249380fb963d83d635db65e6885e54b
Sergio Bocci <powwerie@gmail.com>
2024-12-02 14:43:30 -0700

    classwork

---
+++ Week_14/sortingalgo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/*
+sorting algorithms:
+
+-Different approaches, different complexity + memory req.
+
+Common algorithms:
+- Bubble sort: simple, slow
+- Insertion sort: simple, average case faster than bubble sort
+- quick sort: complicated, fast
+- Merge sort: complicated, fast
+*/
+
+
+int main(){
+
+    return 0;
+}
