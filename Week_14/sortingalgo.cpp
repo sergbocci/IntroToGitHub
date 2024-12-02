2610934818ee6dd5a4693adb2284fb1ec831208a
Sergio Bocci <powwerie@gmail.com>
2024-12-02 15:16:12 -0700

    algorithm intro

--- Week_14/sortingalgo.cpp
+++ Week_14/sortingalgo.cpp
@@ -10,6 +10,32 @@ Common algorithms:
 - Insertion sort: simple, average case faster than bubble sort
 - quick sort: complicated, fast
 - Merge sort: complicated, fast
+
+Bubble sort:
+    Pairwise comparisons
+    Highest value in arrary bubbles to last array position
+    Repeats until data is sorted
+
+Insertion sort:
+    sorts left to right
+    Looks at each element and loops
+    back over what has already been sorted
+    and inserts the element in the correct position
+
+Merge sort:
+    Divide + Conquer algorithm
+    Divide the data into smaller + smaller chunks 
+    Sort the chunks
+    Merge them back together
+
+Quick sort:
+    Divide + Conquer 
+    Chooses a pivot value from the array
+    Everything less than pivot moved left + everything
+     greater than pivot moved right. 
+    Applies to smaller + smaller chunks of array. Selecting pivot and
+        sorting relative to pivot value
+    
 */
 
 
